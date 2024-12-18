8899c047510e9a15c0bc22d0a3b3b572db8fa690
balim <bugrahanbal.bb@gmail.com>
2024-12-18 13:24:25 +0300

    İnternet bağlantısı kesildiğinde websocketten dolayı crash olması bug  fix

--- app/src/main/java/com/balbugrahan/chatbot2024/data/repository/WebSocketRepository.kt
+++ app/src/main/java/com/balbugrahan/chatbot2024/data/repository/WebSocketRepository.kt
@@ -10,37 +10,57 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class WebSocketRepository  @Inject constructor(){
+class WebSocketRepository @Inject constructor() {
 
     private lateinit var webSocketClient: WebSocketClient
     private val _messageLiveData = MutableLiveData<String>()
     val messageLiveData: LiveData<String> = _messageLiveData
+
+    private var isConnected = false // Bağlantı durumunu takip etmek için
 
     fun connectWebSocket() {
         val uri = URI("wss://echo.websocket.org")
         webSocketClient = object : WebSocketClient(uri) {
             override fun onOpen(handshakedata: ServerHandshake?) {
                 Log.d("WebSocket", "Connected")
+                isConnected = true
             }
-
             override fun onMessage(message: String?) {
                 message?.let {
                     _messageLiveData.postValue(it)
                 }
             }
-
             override fun onClose(code: Int, reason: String?, remote: Boolean) {
                 Log.d("WebSocket", "Closed: $reason")
+                isConnected = false
+                handleDisconnected()
             }
-
             override fun onError(ex: Exception?) {
                 Log.e("WebSocket", "Error: ${ex?.message}")
+                isConnected = false
+                handleDisconnected()
             }
         }
         webSocketClient.connect()
     }
-
     fun sendAction(action: String) {
-        webSocketClient.send(action)
+        if (isConnected) {
+            webSocketClient.send(action) // Bağlantı varsa aksiyon çalışsın.Yoksa crash olur.
+        } else {
+            Log.d("WebSocket", "Not connected, can't send action!")
+            handleDisconnected() // Bağlantı yoksa aksiyon alabiliriz.
+        }
+    }
+    private fun handleDisconnected() {
+        // Bağlantı kesilince alınacak aksiyonlar
+        Log.d("WebSocket", "WebSocket disconnected. Reconnecting or alerting user...")
+        // Örnek olarak yeniden bağlanmayı deneyebilir veya kullanıcıya bilgi verebiliriz.
+        //reconnectWebSocket()
+    }
+    private fun reconnectWebSocket() {
+        Log.d("WebSocket", "Attempting to reconnect...")
+        // Yeniden bağlanmayı başlatabiliriz.
+        connectWebSocket()
     }
 }
+
